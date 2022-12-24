e82a7c5c6c9c59c842c3af0435c9d4baca1d274f
Asaf Ben Shabat <asafbs177@gmail.com>
2022-12-24 13:00:33 +0200

    init commit

--- AttendancyApp/ClientApp/src/app/app.component.ts
+++ AttendancyApp/ClientApp/src/app/app.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import * as QRCode from 'qrcode';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  qrCodeData: string;
+  uniqueCode: string;
+  title:string;
+  endpoint:string = "https://www.linkedin.com/in/asaf-yosef-ben-shabat/"
+  constructor(private http: HttpClient) {
+    this.uniqueCode = this.generateUniqueCode();
+  }
+
+  ngOnInit() {
+    this.title = "מבוא למדעי המחשב"
+    this.generateQRCode(this.uniqueCode);
+  }
+
+  generateUniqueCode(): string {
+    // Here should be an endpoint with Student ID
+    return "https://www.linkedin.com/in/asaf-yosef-ben-shabat/";
+  }
+
+  generateQRCode(data: string) {
+    QRCode.toDataURL(data, (err, url) => {
+      this.qrCodeData = url;
+    });
+
+  }
 }
---
+++ AttendancyApp/Controllers/QrCodeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AttendancyApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QrCodeController : Controller
+    {
+        [HttpPost]
+        public IActionResult Post(string data)
+        {
+            // Do something with the QR code data here...
+
+            return Ok();
+        }
+    }
+}
