83c906d25866163b8717cb866606ffd5c002b5b5
Asaf Ben Shabat <asafbs177@gmail.com>
2022-12-24 13:43:06 +0200

    dashboard css n stuff :cheer:

--- AttendancyApp/ClientApp/src/app/app.component.ts
+++ AttendancyApp/ClientApp/src/app/app.component.ts
@@ -2,22 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import * as QRCode from 'qrcode';
+import { style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   qrCodeData: string;
   uniqueCode: string;
   title:string;
+  employees = [];
   endpoint:string = "https://www.linkedin.com/in/asaf-yosef-ben-shabat/"
   constructor(private http: HttpClient) {
     this.uniqueCode = this.generateUniqueCode();
   }
 
   ngOnInit() {
-    this.title = "מבוא למדעי המחשב"
+    this.title = "Computer science 101"
     this.generateQRCode(this.uniqueCode);
   }
 
